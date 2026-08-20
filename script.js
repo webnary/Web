4ced0b1d6b9a772f993cfa40d993b798d9070b47
anas <rabihword@gmail.com>
2026-08-20 20:26:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -112,4 +112,3 @@ document.addEventListener('keydown', e=>{
 window.addEventListener('resize', ()=>{
   if(window.innerWidth > 980 && navEl.classList.contains('menu-open')) closeMenu();
 });
-<script>!function(w,d,s,u,n,a,b){if(w[n])return;a=w[n]={q:[],t:+new Date,s:[],o:u,track:function(){a.q.push([+new Date].concat([].slice.call(arguments)))},setScope:function(){a.s=[].slice.call(arguments).filter(function(x){return typeof x==="string"});a.q.push([+new Date,"setScope"].concat(a.s))},scope:function(){var c=[].slice.call(arguments);return{track:function(){a.q.push([+new Date].concat([].slice.call(arguments)).concat([{__scope:c}]))}}}};b=d.createElement(s);b.async=1;b.src=u+"/s.js";d.getElementsByTagName(s)[0].parentNode.insertBefore(b,d.getElementsByTagName(s)[0])}(window,document,"script","https://t.whop.tw","whop");whop.setScope("biz_PCMrsFLEKKFuCs");whop.track("page");</script>
